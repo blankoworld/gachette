6eabc51e285b75dc6e791438e01cb74e3d6fbea0
Olivier DOSSMANN <git@dossmann.net>
2019-03-04 23:17:02 +0100

    Add simple way to check if post request comes from Github/Gitlab/Gitea

--- src/gachette.cr
+++ src/gachette.cr
@@ -1,8 +1,25 @@
 # TODO: Write documentation for `Gachette`
 require "kemal"
+require "json"
 
 get "/" do
   "J'ai la gachette facile !"
+end
+
+post "/" do |env|
+  result = "Message reçu."
+  head = env.request.headers
+  agent = head.fetch("User-Agent", "None")
+  if agent == "GiteaServer" && head.has_key?("X-Gitea-Event")
+    result += "\ndemande Gitea"
+  elsif agent.starts_with?("GitHub-Hookshot/") && head.has_key?("X-Github-Event")
+    result += "\ndemande Github"
+  elsif head.has_key?("X-Gitlab-Event")
+    result += "\ndemande Gitlab"
+  else
+    result += "\ndemande inconnue"
+  end
+  result
 end
 
 Kemal.run
