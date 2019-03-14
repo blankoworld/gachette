71152652bb02bf3c8acf1361ae8aef2592dedcd6
Olivier DOSSMANN <git@dossmann.net>
2019-03-14 19:16:49 +0100

    Add more options in Kemal config to be accessible

--- src/gachette.cr
+++ src/gachette.cr
@@ -1,4 +1,4 @@
-require "kemal"
+require "./kemal.cr"
 require "json"
 require "./gachette/helpers/*"
 require "./gitea.cr"
@@ -9,54 +9,7 @@ require "./gitlab.cr"
 # specific commands on specific repositories.
 
 # variables initialization
-kind = nil       # Payload kind: gitea or github or gitlab
-namespace = nil  # Namespace from which you receive a payload
-command = nil    # Command to run
-scriptfile = nil # Script to launch
-secretkey = nil  # secret key
 ALLOWED_KINDS = ["gitea", "github", "gitlab"]
-
-# redefines Kemal CLI options
-Kemal.config.extra_options do |parser|
-  parser.banner = "\
-Usage: gachette -k [gitea/github/gitlab] -n user/namespace [arguments]
-Example: gachette -k github -n blankoworld/gachette -f script.sh"
-
-  # payload kind: gitea or github or gitlab
-  parser.on("-k name", "--kind name", "Kind of webhook: gitea, gitlab, github") do |name|
-    kind = name
-  end
-
-  # namespace from which you will receive the payload
-  parser.on("-n name", "--name name", "User's namespace. Ex. olivier/gachette") do |name|
-    namespace = name
-  end
-
-  # command: run specific command for the given namespace
-  parser.on("-c name", "--command name", "Command to run") do |name|
-    command = name
-  end
-
-  # file: script that content some commands to run for the given namespace
-  parser.on("-f name", "--file name", "Script to run") do |name|
-    unless Dir.exists? name
-      abort "#{name} file does not exist!"
-    end
-    scriptfile = name
-  end
-
-  parser.missing_option do |flag|
-    STDERR.puts "WARNING: #{flag} should be set."
-    STDERR.puts parser
-    exit(1)
-  end
-
-  parser.invalid_option do |flag|
-    STDERR.puts "ERROR: #{flag} is not a valid option."
-    STDERR.puts parser
-    exit(1)
-  end
-end
 
 # `Payload` content is what the remote hook server gives us.
 # It describes data we could use to find which command we should use.
@@ -95,45 +48,44 @@ end
 post "/" do |env|
 
   # Stop process if no kind found
-  if !kind
+  if !Kemal.config.kind
     log("unknown kind of payload received")
-    puts Kemal.config.extra_options
     next
   end
 
-  payload = Payload.new kind, env.request  
-  log("#{kind} payload received: #{payload.project}")
+  payload = Payload.new Kemal.config.kind, env.request  
+  log("#{Kemal.config.kind} payload received: #{payload.project}")
 
-  kind
+  Kemal.config.kind
 end
 
 # Start service as Kemal one
 # First check mandatories options
 Kemal.run do |config|
   # miscellaneous initialization
-  secretkey = ENV["GACHETTE_KEY"] if ENV.has_key?("GACHETTE_KEY")
+  config.secretkey = ENV["GACHETTE_KEY"] if ENV.has_key?("GACHETTE_KEY")
   if kemal_env = ENV["KEMAL_ENV"]?
       if kemal_env == "test"
-        kind = "github"
-        namespace = "blankoworld/gachette"
-        command = "echo \"Testing env. with github:blankoworld/gachette\""
+        config.kind = "github"
+        config.namespace = "blankoworld/gachette"
+        config.command = "echo \"Testing env. with github:blankoworld/gachette\""
     end
   end
 
   # mandatories options
-  if !kind
+  if !config.kind
     puts "kind option missing!"
     exit(1)
-  elsif !ALLOWED_KINDS.includes?(kind)
-    puts "`#{kind}` kind is not allowed! Use on of: #{ALLOWED_KINDS}"
+  elsif !ALLOWED_KINDS.includes?(config.kind)
+    puts "`#{config.kind}` kind is not allowed! Use on of: #{ALLOWED_KINDS}"
     exit (1)
   end
-  if !namespace
+  if !config.namespace
     puts "name option is missing!"
     exit (1)
   end
-  if !command && !scriptfile
-    puts "need something to run!"
+  if !config.command && !config.scriptfile
+    puts "need something to run! Either a command or a scriptfile."
     exit (1)
   end
 end
---
+++ src/kemal.cr
@@ -0,0 +1,59 @@
+require "kemal"
+
+# redefines Kemal Config class to add kind, namespace, command, scriptfile
+# and secretkey properties!
+class Kemal::Config
+  @kind : String = "github"
+  @namespace : String = "blankoworld/gachette"
+  @command : (String)?
+  @scriptfile : (String)?
+  @secretkey : (String)?
+
+  property kind, namespace, command, scriptfile, secretkey
+
+  def initialize(@serve_static, @host_binding, @port, @env, @public_folder, @logging, @logger, @error_handler, @always_rescue, @router_included, @default_handlers_setup, @running, @shutdown_message, @handler_position)
+  end
+end
+
+# redefines Kemal CLI options
+Kemal.config.extra_options do |parser|
+  config = Kemal.config
+  parser.banner = "\
+Usage: gachette -k [gitea/github/gitlab] -n user/namespace [arguments]
+Example: gachette -k github -n blankoworld/gachette -f script.sh"
+
+  # payload kind: gitea or github or gitlab
+  parser.on("-k name", "--kind name", "Kind of webhook: gitea, gitlab, github") do |name|
+    config.kind = name
+  end
+
+  # namespace from which you will receive the payload
+  parser.on("-n name", "--name name", "User's namespace. Ex. olivier/gachette") do |name|
+    config.namespace = name
+  end
+
+  # command: run specific command for the given namespace
+  parser.on("-c name", "--command name", "Command to run") do |name|
+    config.command = name
+  end
+
+  # file: script that content some commands to run for the given namespace
+  parser.on("-f name", "--file name", "Script to run") do |name|
+    unless Dir.exists? name
+      abort "#{name} file does not exist!"
+    end
+    config.scriptfile = name
+  end
+
+  parser.missing_option do |flag|
+    STDERR.puts "WARNING: #{flag} should be set."
+    STDERR.puts parser
+    exit(1)
+  end
+
+  parser.invalid_option do |flag|
+    STDERR.puts "ERROR: #{flag} is not a valid option."
+    STDERR.puts parser
+    exit(1)
+  end
+end
