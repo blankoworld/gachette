fb35c202e2a8c3eff7806e6b777824760a5e8cc3
Olivier DOSSMANN <git@dossmann.net>
2019-08-13 21:42:24 +0200

    Refactorization - move Payload object to a specific file

---
+++ spec/payload_spec.cr
@@ -0,0 +1,71 @@
+require "spec-kemal"
+require "./spec_helper.cr"
+require "../src/kemal.cr"
+
+describe "Payload object" do
+  it "creates a payload GITEA object" do
+    headers = HTTP::Headers{
+      "User-Agent" => "GiteaServer",
+      "Content-Type" => "application/json",
+			"X-GitHub-Delivery" => "e905e09b-6b35-46b4-bba5-c6599d21ab33",
+			"X-GitHub-Event" => "push",
+			"X-Gitea-Delivery" => "e905e09b-6b35-46b4-bba5-c6599d21ab33",
+			"X-Gitea-Event" => "push",
+			"X-Gogs-Delivery" => "e905e09b-6b35-46b4-bba5-c6599d21ab33",
+			"X-Gogs-Event" => "push             "}
+    body = open_json_file("spec/push_gitea.json")
+    request = HTTP::Request.new(
+      "POST",
+      "/",
+      body: body,
+      headers: headers,
+    )
+    payload = Payload.new request
+    payload.kind.should eq "gitea"
+    payload.content.should eq body
+    payload.secret.should eq "3gEsCfjlV2ugRwgpU#w1*WaW*wa4NXgGmpCfkbG3"
+    payload.project.should eq "gitea/webhooks"
+  end
+
+  it "creates a payload GITLAB object" do
+    headers = HTTP::Headers{
+      "Content-Type" => "application/json",
+      "X-Gitlab-Event" => "Push Hook",
+      "X-Gitlab-Token" => "mot2passe"}
+    body = open_json_file("spec/push_gitlab.json")
+    request = HTTP::Request.new(
+      "POST",
+      "/",
+      body: body,
+      headers: headers,
+    )
+    payload = Payload.new request
+    payload.kind.should eq "gitlab"
+    payload.content.should eq body
+    payload.secret.should eq "mot2passe"
+    payload.project.should eq "mike/diaspora"
+  end
+
+  it "creates a payload GITHUB object" do
+    headers = HTTP::Headers{
+			"content-type" => "application/json",
+			"Expect" => "",
+			"User-Agent" => "GitHub-Hookshot/hashsah",
+			"X-GitHub-Delivery" => "691df212-bdb7-11e9-8119-fa2f1de358a6",
+			"X-GitHub-Event" => "push",
+			"X-Hub-Signature" => "sha1=8cfbbbf7a0b7b8322519ac7fbf346ad9a9475236"}
+		body = GITHUB_BODY
+    request = HTTP::Request.new(
+      "POST",
+      "/",
+      body: body,
+      headers: headers,
+    )
+    payload = Payload.new request
+    payload.kind.should eq "github"
+    payload.content.should eq body
+    payload.secret.should eq "sha1=8cfbbbf7a0b7b8322519ac7fbf346ad9a9475236"
+    payload.project.should eq "blankoworld/gachette"
+  end
+
+end
---
+++ src/payload.cr
@@ -0,0 +1,64 @@
+require "./gachette/helpers/*"
+require "./gitea.cr"
+require "./github.cr"
+require "./gitlab.cr"
+
+# `Payload` content is what the remote hook server gives us.
+# It describes data we could use to find which command we should use.
+# Properties:
+# - kind:     payload type. For an example: gitea, github or gitlab
+# - project:  project name(space).
+# - secret:   shared secret to check you're allowed to launch the command/script
+class Payload
+  @kind : String
+  @project : String = "unknown"
+  @secret : String = ""
+  @content : String
+  def initialize(req : HTTP::Request)
+    # request kind
+    request_kind = request_type(req.headers).to_s
+    @kind = request_kind == "" ? "unknown" : request_kind
+    # content
+    @content = req.body.not_nil!.gets_to_end.to_s
+    case @kind
+      when "gitea"
+        gitea = Gitea::Payload.from_json(@content)
+        @project = gitea.repository.full_name
+        @secret = gitea.secret
+      when "github"
+        github = Github::Payload.from_json(@content)
+        @project =  github.repository.full_name
+        hash = req.headers.fetch("X-Hub-Signature", "None")
+        if hash != "None"
+          @secret = hash.to_s
+        end
+      when "gitlab"
+        gitlab = Gitlab::Payload.from_json(@content)
+        @project = gitlab.project.path_with_namespace
+        token = req.headers.fetch("X-Gitlab-Token", "None")
+        if token != "None"
+          @secret = token.to_s
+        end
+    end
+  end
+
+  # namespace of given project. Example: blankoworld/gachette
+  def project
+    @project
+  end
+
+  # kind of payload.
+  def kind
+    @kind
+  end
+
+  # special secret from project
+  def secret
+    @secret
+  end
+
+  # payload content (body)
+  def content
+    @content
+  end
+end
